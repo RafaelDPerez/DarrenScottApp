8ddd77ce8a87a27c2d60d53ddafa93be7ac8808a
Maria del Carmen Peignand <mariapeignand@maricarmens-macbook-pro.local>
2016-10-27 23:29:32 -0400

    Initial Commit.

--- DarrenScottSalonApp/ViewController.h
+++ DarrenScottSalonApp/ViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ASStarRatingView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+{
+    UIImagePickerController *ipc;
+    UIPopoverController *popover;
+}
 
+@property (weak, nonatomic) IBOutlet UIButton *btnGallery;
+@property (weak, nonatomic) IBOutlet UIButton *btnCamera;
+@property (weak, nonatomic) IBOutlet UIButton *btnPhotoGallery;
+@property(strong,nonatomic) IBOutlet ASStarRatingView *starsRating;
+@property (weak, nonatomic) IBOutlet UIImageView *ivPickedImage;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
--- DarrenScottSalonApp/ViewController.m
+++ DarrenScottSalonApp/ViewController.m
@@ -8,20 +8,119 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+   // FIRStorageReference *storageRef;
+    NSData *imageData;
+    UIImage *hola;
+}
 
 @end
 
 @implementation ViewController
-
+@synthesize ivPickedImage, textField;
+@synthesize btnCamera,btnGallery,btnPhotoGallery;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGRect frameRect = textField.frame;
+    frameRect.size.height = 100; // <-- Specify the height you want here.
+    textField.frame = frameRect;
     // Do any additional setup after loading the view, typically from a nib.
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected)];
+    singleTap.numberOfTapsRequired = 1;
+    [ivPickedImage setUserInteractionEnabled:YES];
+    [ivPickedImage addGestureRecognizer:singleTap];
 }
 
-- (void)didReceiveMemoryWarning {
+
+-(void)tapDetected{
+    ipc= [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    ipc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
+        [self presentViewController:ipc animated:YES completion:nil];
+    else
+    {
+        popover=[[UIPopoverController alloc]initWithContentViewController:ipc];
+        [popover presentPopoverFromRect:btnGallery.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
+- (IBAction)btnGalleryClicked:(id)sender
+{
+    ipc= [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    ipc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
+        [self presentViewController:ipc animated:YES completion:nil];
+    else
+    {
+        popover=[[UIPopoverController alloc]initWithContentViewController:ipc];
+        [popover presentPopoverFromRect:btnGallery.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
+
+- (IBAction)btnPhotoGalleryClicked:(id)sender
+{
+    ipc= [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    ipc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
+        [self presentViewController:ipc animated:YES completion:nil];
+    else
+    {
+        popover=[[UIPopoverController alloc]initWithContentViewController:ipc];
+        [popover presentPopoverFromRect:btnGallery.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
+
+
+- (IBAction)btnCameraClicked:(id)sender
+{
+    ipc = [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        ipc.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:ipc animated:YES completion:NULL];
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"" message:@"No Camera Available." delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+        alert = nil;
+    }
+}
+
+#pragma mark - ImagePickerController Delegate
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone) {
+        [picker dismissViewControllerAnimated:YES completion:nil];
+    } else {
+        [popover dismissPopoverAnimated:YES];
+    }
+    ivPickedImage.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    imageData = UIImageJPEGRepresentation([info objectForKey:UIImagePickerControllerOriginalImage], 1.0);
+    //hola = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
